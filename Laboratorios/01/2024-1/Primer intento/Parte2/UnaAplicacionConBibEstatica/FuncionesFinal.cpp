932a40cf97611d9d32962f686d976a46d59f57dc
HogRidee <iaraoz061102@gmail.com>
2024-08-21 11:26:38 -0500

    Continue practice for lab01

--- Laboratorios/01/2024-1/Primer intento/Parte2/UnaAplicacionConBibEstatica/FuncionesFinal.cpp
+++ Laboratorios/01/2024-1/Primer intento/Parte2/UnaAplicacionConBibEstatica/FuncionesFinal.cpp
@@ -15,6 +15,7 @@ void leerClientes(ifstream &archClientes, struct Cliente *arrClientes){
     int n = 0;
     while(true){
         if(not (archClientes >> arrClientes[n])){
+            arrClientes[n].dni = 0;
             break;
         }
         n++;
@@ -40,9 +41,21 @@ void leerPedidos(ifstream &archPedidos, struct Libro *&arrLibros,
             }
             buffer[7] = '\0';
             strcpy(libroPedido.codigoDelLibro, buffer);
+            procesarPedido(libroPedido, arrLibros, arrClientes, dni);
             archPedidos.get(c);
             if(c == '\n' or archPedidos.eof()) break;
             else archPedidos >> ws;
+        }
+    }
+}
+
+void procesarPedido(struct LibroSolicitado libroPedido, struct Libro *&arrLibros, 
+        struct Cliente *&arrClientes, int dni){
+    if(libroPedido >> arrLibros){
+        for(int i = 0; arrClientes[i].dni != 0; i++){
+            if(arrClientes[i].dni == dni){
+                
+            }
         }
     }
 }
--- Laboratorios/01/2024-1/Primer intento/Parte2/UnaAplicacionConBibEstatica/FuncionesFinal.h
+++ Laboratorios/01/2024-1/Primer intento/Parte2/UnaAplicacionConBibEstatica/FuncionesFinal.h
@@ -21,6 +21,9 @@ void leerLibros(ifstream &archLibros, struct Libro *arrLibros);
 void leerClientes(ifstream &archClientes, struct Cliente *arrClientes);
 void leerPedidos(ifstream &archPedidos, struct Libro *&arrLibros, 
         struct Cliente *&arrClientes);
+void procesarPedido(struct LibroSolicitado libroPedido, struct Libro *&arrLibros, 
+        struct Cliente *&arrClientes, int dni);
+
 
 #endif /* FUNCIONESFINAL_H */
 
